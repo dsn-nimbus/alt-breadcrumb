5f43477524ff056d4ddb6e992337efeb0e3d389d
Leonardo Esteves Barboza de Sant'anna <leonardoebs@gmail.com>
2018-02-21 09:42:18 -0300

    v0.0.2

--- dist/alt-breadcrumb.js
+++ dist/alt-breadcrumb.js
@@ -4,6 +4,7 @@
   angular
   .module('alt.breadcrumb', [])
     .constant('AltBreadcrumbEventos', {
+      EVENTO_BREADCRUMB_PRONTO: 'alt.breadcrumb_pronto',
       EVENTO_CRIACAO_BREADCRUMB: 'alt.criar_breadcrumb',
       EVENTO_NAVEGACAO_SOLICITADA: 'alt.navegacao_solicitada',
       EVENTO_DESTRUICAO_BREADCRUMB: 'alt.destruir_breadcrumb'
@@ -14,39 +15,69 @@
       };
     }])
     .directive('altBreadcrumb', ['$rootScope', '$sce', 'AltBreadcrumbEventos', function($rootScope, $sce, AltBreadcrumbEventos) {
-      var _templateBreadcrumb = '';
-      var _descricaoEtapaAtual = '';
+      var _mainDescription = '';
       var _template = `<div id="alt-breadcrumb">
-                         <div class="breadcrumb-itens-container">
-                            {{_templateBreadcrumb}}
+                         <div class="breadcrumb-items-container">
+                           <div class="breadcrumb-item text-center"
+                                ng-class="{'breadcrumb-selected-item': step.isActive}"
+                                ng-click="_onClick(step)"
+                                ng-repeat="step in _obj.steps">
+                             <p ng-show="_obj.showNumbers">{{$index + 1}}</p>
+                             <p ng-show="!_obj.showNumbers && step.isCompleted" class="fa fa-fw fa-check"></p>
+                             <p ng-show="!_obj.showNumbers && !step.isCompleted" class="fa fa-fw fa-minus"></p>
+                           </div>
             			       </div>
-                  			 <div class="breadcrumb-descricao-container text-center" ng-show="showDescription">
-                  			    <span ng-bind="_descricaoEtapaAtual"></span>
+                  			 <div class="breadcrumb-description-container text-center" ng-show="_obj.showDescription">
+                  			    <span>{{_mainDescription}}</span>
                   			 </div>
                        </div>`;
 
       var _link = function(scope, element, attrs) {
         var _breadcrumb = element.find('#alt-breadcrumb');
+        scope._obj = {
+          showNumbers: false,
+          showDescription: false,
+          steps: []
+        };
 
-        $scope.$on(AltBreadcrumbEventos.EVENTO_CRIACAO_BREADCRUMB, function(evento, obj) {
+        scope._onClick = function(step) {
+          scope.$emit(AltBreadcrumbEventos.EVENTO_NAVEGACAO_SOLICITADA, step);
+        };
+
+        scope.$on(AltBreadcrumbEventos.EVENTO_CRIACAO_BREADCRUMB, function(evento, obj) {
           if (!angular.isObject(obj)) {
             throw new Error('obj was not informed.');
           }
 
           scope._obj = obj;
 
-          if (!!scope._obj && !!scope._obj.etapas) {
-            for (var i = 0; i < scope._obj.etapas.length; i++) {
-              _templateBreadcrumb += `<div class="breadcrumb-item text-center">
-                                					<p ng-show="_obj.showNumbers">${i + 1}</p>
-                                				</div>`;
+          for (var i = 0; i < scope._obj.steps.length; i++) {
+            scope._obj.steps[i].index = i;
+            if(!!scope._obj.steps[i].isActive) {
+              scope._mainDescription = scope._obj.steps[i].description;
             }
           }
         });
 
-        $scope.$on(AltBreadcrumbEventos.EVENTO_DESTRUICAO_BREADCRUMB, function(evento, obj) {
+        scope.$on(AltBreadcrumbEventos.EVENTO_NAVEGACAO_REALIZADA, function(evento, index) {
+          if (index < 0) {
+            throw new Error('The given index is not valid.')
+          }
+
+          for (var i = 0; i < scope._obj.steps.length; i++) {
+            scope._obj.steps[i].isActive = false;
+            if(scope._obj.steps[i].index === index) {
+              scope._obj.steps[i].isActive = true;
+              scope._mainDescription = scope._obj.steps[i].description;
+            }
+          }
+        });
+
+        scope.$on(AltBreadcrumbEventos.EVENTO_DESTRUICAO_BREADCRUMB, function(evento, obj) {
           _breadcrumb = undefined;
         });
+
+        scope.$emit(AltBreadcrumbEventos.EVENTO_BREADCRUMB_PRONTO);
       }
 
       var _scope = {};
